13618284ae8649f1e290ec07b1c3a30d0c3a6331
Rafael Cisneros <rafael.cisneros@aist.go.jp>
2019-09-13 17:13:23 +0900

    Measured time for Lambert

--- lambertW/foo.cpp
+++ lambertW/foo.cpp
@@ -6,15 +6,22 @@
 #include "LambertW.h"
 #include <iostream>
 #include <iomanip>
+#include <time.h>
 
 int main()
 {
   std::cout << std::setprecision(10);
   
   double z = 3.14;
-  double r = utl::LambertW<0>(z);
-  std::cout << "lambert_w0(z) = " << r << std::endl;
 
+  clock_t time = clock();
+  double r = utl::LambertW<0>(z);
+  time = (int) (clock() - time);
+
+  std::cout << "lambert_w0(z) = " << r << std::endl;
+  std::cout << "Computed in " << time << std::endl;
+  std::cout << "CLOCKS_PER_SEC = " << CLOCKS_PER_SEC << std::endl;
+  
   return 0;
 }
 
